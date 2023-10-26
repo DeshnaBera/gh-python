c4af997feb332641b2b943ab0df318181785da48
Bera <2121490@cognizant.com>
2023-10-26 12:33:41 +0530

    modified code

--- sum.py
+++ sum.py
@@ -1,7 +1,8 @@
 def add_numbers(a, b):
     return a + b
 
-num1 = 12
-num2 = 13
-result = add_numbers(num1, num2)
-print(f"The sum of {num1} and {num2} is {result}")
+if len(sys.argv) == 3:
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+    result = add_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is {result}")
