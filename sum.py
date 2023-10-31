a6dd2e53354fac20d1299ea9b9433bc4acb04da2
Bera <2121490@cognizant.com>
2023-10-31 10:46:15 +0530

    changed sum.py

--- sum.py
+++ sum.py
@@ -7,4 +7,4 @@ if len(sys.argv) == 3:
     num1 = int(sys.argv[1])
     num2 = int(sys.argv[2])
     result = add_numbers(num1, num2)
-    print(f"The sum of {num1} and {num2} is {result}")
+    print(result)
