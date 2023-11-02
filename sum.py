ebd8efdb8b61a7bf97f4e9afe01d0fe3b5098032
Bera <2121490@cognizant.com>
2023-11-02 13:13:39 +0530

    added try and except block

--- sum.py
+++ sum.py
@@ -4,7 +4,12 @@ def add_numbers(a, b):
     return a + b
 
 if len(sys.argv) == 3:
-    num1 = int(sys.argv[1])
-    num2 = int(sys.argv[2])
-    result = add_numbers(num1, num)
-    print(result)
+    try:
+        num1 = int(sys.argv[1])
+        num2 = int(sys.argv[2])
+        result = add_numbers(num1, num)
+        print(result)
+    except Exception as e:
+        sys.exit(1)
+    else:
+        sys.exit(0)
