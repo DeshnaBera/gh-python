0dc070bc3436641adb0ba20b66f20d6d0f9edfbb
Bera <2121490@cognizant.com>
2023-11-02 13:16:20 +0530

    removed changes

--- sum.py
+++ sum.py
@@ -4,12 +4,7 @@ def add_numbers(a, b):
     return a + b
 
 if len(sys.argv) == 3:
-    try:
         num1 = int(sys.argv[1])
         num2 = int(sys.argv[2])
         result = add_numbers(num1, num)
         print(result)
-    except Exception as e:
-        sys.exit(1)
-    else:
-        sys.exit(0)
