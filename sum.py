812b8adb9eecae00bec73dd16d328b01d0484af3
DeshnaBera <beradeshna@gmail.com>
2023-12-11 15:44:09 +0530

    fixed code

--- sum.py
+++ sum.py
@@ -1,10 +1,10 @@
-import sys
+
 
 def add_numbers(a, b):
     return a + b
 
 if len(sys.argv) == 3:
-        num1 = int(sys.argv[1])
-        num2 = int(sys.argv[2])
+        num1 = 4
+        num2 = 5
         result = add_numbers(num, num2)
         print(result)
